d13e2c98037d412acaf4dc7880948eccba2f0f93
Crni Bombarder <fumeron.remi@gmail.com>
2025-01-25 18:53:23 +0100

    Bugfix / Prepare some tests for the Websocket

--- src/websocket.py
+++ src/websocket.py
@@ -5,29 +5,23 @@ from quart import Quart, request
 from hypercorn.config import Config
 from hypercorn.asyncio import serve
 
-from src.watched_refs import Refs
-
 class HttpsServer:
 
     GITHUBHOOK_TARGET = "/github"
+    FORMS_TARGET = "/forms"
 
-    def __init__(self, watched_refs_file, secret_file=None):
+    def __init__(self, secret_file=None):
         self.secret_file = secret_file
-        self.refs = Refs(watched_refs_file)
-        self.server = Quart("Github_Webhook_Server")
-
-        def add_refs(self, new_refs):
-            
+        self.server = Quart("Discord_Webhook_Server")
 
         @self.server.route(HttpsServer.GITHUBHOOK_TARGET, methods = ["POST"])
         async def post_handler():
             if request.method =="POST":
-                print("ok")
-                return "Ok"
-    
+                print(await request.get_json())
+
     async def run(self, port=8080):
         config = Config()
-        config.bind = [f"localhost:{port}"]
+        config.bind = [f"0.0.0.0:{port}"]
         server_task = asyncio.create_task(serve(self.server, config))
         print("The server is launched")
         await server_task
